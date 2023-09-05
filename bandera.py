2b60cd0f2d6bcccfe51732167fda4a9bb7a45d79
Thiago <Thiago.encina@sagradoalcal.edu.ar>
2023-09-05 17:05:43 -0300

    Bandera agregada a la ventana

--- bandera.py
+++ bandera.py
@@ -1,28 +1,58 @@
 import tkinter as tk
-    
+import requests
+from io import BytesIO
+from PIL import Image, ImageTk
+from lectorCSV import sorteador 
+
 ventana = tk.Tk()
 
-ventana.title("Adivinanzas")
+def mostrar_imagen_desde_url(url):
+    response = requests.get(url)
+    image_data = BytesIO(response.content)
+    image = Image.open(image_data)
+    photo = ImageTk.PhotoImage(image)
+    label_imagen.config(image=photo)
+    label_imagen.image = photo 
 
-def center_widget(widget, container):
-    widget.update_idletasks()
-    width = widget.winfo_width()
-    height = widget.winfo_height()
-    x = (container.winfo_width() - width) // 2
-    y = (container.winfo_height() - height) // 2
-    widget.place(x=70, y=40)
+frame1 = tk.Frame(ventana, bg="white", width=256, height=192)
+frame1.grid(column=0, row=0)
 
-ventana.configure(bg="white")
-ventana.resizable(width=False, height=False)
-frame2 = tk.Frame(ventana, bg="red", width=256, height=192)
-boton2 = tk.Button(ventana, text="", width=10, height=5)
-boton2.grid(column=0, row=2)
-frame2.grid(column=0, row=2)
+label_imagen = tk.Label(frame1)
+label_imagen.grid(column=0, row=0)
 
-frame1 = tk.Frame(ventana, bg="red", width=256, height=192)
+frame2 = tk.Frame(ventana, bg="blue", width=256, height=192)
+boton1 = tk.Button(frame2, text="", width=7, height=1)
+boton2 = tk.Button(frame2, text="", width=7, height=1)
+boton3 = tk.Button(frame2, text="", width=7, height=1)
+boton4 = tk.Button(frame2, text="", width=7, height=1)
+boton1.grid(column=0, row=0)
+boton2.grid(column=0, row=1)
+boton3.grid(column=1, row=0)
+boton4.grid(column=1, row=1)
+
+'''.place(x=15, y=25)
+.place(x=15, y=70)
+.place(x=150, y=25)
+.place(x=150, y=70)'''
+frame2.grid(column=0, row=1)
 
 ventana.geometry("250x320")
 
-center_widget(boton2,ventana)
+banderas = []
+for i in range(4):
+    bandera = sorteador()
+    banderas.append(bandera)
+ 
+boton1.config(text=banderas[0][0])   
+boton2.config(text=banderas[1][0])
+boton3.config(text=banderas[2][0])
+boton4.config(text=banderas[3][0])
+    
+url_imagen = f"https://flagcdn.com/256x192/{banderas[3][1].lower()}.png"
+mostrar_imagen_desde_url(url_imagen) 
+
+boton = [boton1, boton2, boton3, boton4]
+
+
 
 ventana.mainloop()
